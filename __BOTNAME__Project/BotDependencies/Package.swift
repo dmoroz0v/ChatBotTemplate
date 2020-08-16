c8660717dcb815a00803774607755ca80ad70646
Denis Morozov <denis.moroz.morozov@gmail.com>
2020-08-16 21:42:06 +0300

    Add __TAG__

--- __BOTNAME__Project/BotDependencies/Package.swift
+++ __BOTNAME__Project/BotDependencies/Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         .package(path: "../../__BOTNAME__"),
-        .package(url: "https://github.com/dmoroz0v/TgBotSDK.git", from: "0.0.2"),
+        .package(url: "https://github.com/dmoroz0v/TgBotSDK.git", .exact("__TAG__")),
     ],
     targets: [
         .target(
